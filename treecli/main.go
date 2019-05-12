8f6f5df0d2036ae19b0a4cc05d1fb65e328e7264
Akavith <32568502+Akavith@users.noreply.github.com>
2019-05-12 14:03:58 +0200

    add example remote actor architecture

--- treecli/main.go
+++ treecli/main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/remote"
+	"github.com/ob-vss-ss19/blatt-3-stiglmeier/messages"
+	"sync"
+	"time"
 )
 
+type MyActor struct {
+	count int
+}
+
+func (state *MyActor) Receive(context actor.Context) {
+	switch msg := context.Message().(type) {
+	case *messages.Response:
+		state.count++
+		fmt.Println(msg.SomeValue + string(state.count))
+	default:
+		fmt.Printf("unknown message type\n")
+	}
+}
+
 func main() {
-	fmt.Println("Hello Tree-CLI!")
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	remote.Start("localhost:8090")
+
+	context := actor.EmptyRootContext
+	props := actor.PropsFromProducer(func() actor.Actor { return &MyActor{} })
+	pid := context.Spawn(props)
+	message := &messages.Echo{Message: "hej", Sender: 55}
+
+	//this is to spawn remote actor we want to communicate with
+	spawnResponse, _ := remote.SpawnNamed("localhost:8091", "myactor", "hello", time.Second)
+
+	// get spawned PID
+	spawnedPID := spawnResponse.Pid
+	context.RequestWithCustomSender(spawnedPID, message, pid)
+	//context.Send(spawnedPID, message)
+
+	wg.Wait()
 }
--- treeservice/main.go
+++ treeservice/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/remote"
+	"github.com/ob-vss-ss19/blatt-3-stiglmeier/messages"
+	"sync"
 )
 
 func main() {
 	fmt.Println("Hello Tree-Service!")
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	remote.Start("localhost:8091")
+
+	remote.Register("hello", actor.PropsFromProducer(func() actor.Actor { return &MyActor{} }))
+	wg.Wait()
+}
+
+type MyActor struct{}
+
+func (*MyActor) Receive(context actor.Context) {
+	switch context.Message().(type) {
+	case *messages.Echo:
+		context.Respond(&messages.Response{SomeValue: "message received"})
+		fmt.Printf("responding...")
+	default:
+		fmt.Printf("unknown message type\n")
+	}
 }
