b16227b96527128fb61dcec0e3d5651db74cb2ea
Akavith <32568502+Akavith@users.noreply.github.com>
2019-05-13 11:00:58 +0200

    create node actor

--- tree/node.go
+++ tree/node.go
@@ -1,1 +1,17 @@
 package tree
+
+import "github.com/AsynkronIT/protoactor-go/actor"
+
+type NodeActor struct {
+	LeafSize   int
+	LeftNode   *actor.PID
+	RightNode  *actor.PID
+	LeftMaxKey int
+	Values     map[int]string
+}
+
+func (state *NodeActor) Receive(context actor.Context) {
+	//switch msg := context.Message().(type) {
+	//case :
+
+}
