61e7a00b3a1bbbeb2f09ed53b2cc3d73694405e2
Akavith <32568502+Akavith@users.noreply.github.com>
2019-05-13 11:30:34 +0200

    create cli

--- treecli/main.go
+++ treecli/main.go
@@ -1,46 +1,143 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/log"
 	"github.com/AsynkronIT/protoactor-go/remote"
 	"github.com/ob-vss-ss19/blatt-3-stiglmeier/messages"
+	"strconv"
 	"sync"
 	"time"
 )
 
-type MyActor struct {
-	count int
+type CliActor struct {
 }
 
-func (state *MyActor) Receive(context actor.Context) {
+func (state *CliActor) Receive(context actor.Context) {
 	switch msg := context.Message().(type) {
-	case *messages.Response:
-		state.count++
-		fmt.Println(msg.SomeValue + string(state.count))
-	default:
-		fmt.Printf("unknown message type\n")
+	case *messages.Success:
+		fmt.Println("Success")
+		wg.Done()
+	case *messages.Failure:
+		fmt.Printf("Failure: " + msg.Cause + "\n")
+		wg.Done()
+	case *messages.TraverseResult:
+		fmt.Printf("Key: %d, Value: %s\n", msg.Key, msg.Value)
+		if msg.Last {
+			wg.Done()
+		}
+	case *messages.TreeData:
+		fmt.Printf("New Tree[id: %d, token: %s]", msg.Id, msg.Token)
+		wg.Done()
 	}
 }
 
+var (
+	flagBind   = flag.String("bind", "localhost:8090", "local adress:port")
+	flagRemote = flag.String("remote", "localhost:8091", "remote adress:port")
+
+	id          = flag.Int("id", 0, "tree id")
+	token       = flag.String("token", "", "tree token")
+	forceDelete = flag.Bool("force-delete", false, "delete tree")
+
+	context    *actor.RootContext
+	pid        *actor.PID
+	spawnedPID *actor.PID
+	wg         sync.WaitGroup
+)
+
 func main() {
-	var wg sync.WaitGroup
 	wg.Add(1)
+	flag.Parse()
+	remote.SetLogLevel(log.ErrorLevel)
+	remote.Start(*flagBind)
 
-	remote.Start("localhost:8090")
+	context = actor.EmptyRootContext
+	props := actor.PropsFromProducer(func() actor.Actor { return &CliActor{} })
+	pid = context.Spawn(props)
 
-	context := actor.EmptyRootContext
-	props := actor.PropsFromProducer(func() actor.Actor { return &MyActor{} })
-	pid := context.Spawn(props)
-	message := &messages.Echo{Message: "hej", Sender: 55}
+	spawnResponse, _ := remote.SpawnNamed(*flagRemote, "serviceactor", "treeservice", time.Second)
+	spawnedPID = spawnResponse.Pid
 
-	//this is to spawn remote actor we want to communicate with
-	spawnResponse, _ := remote.SpawnNamed("localhost:8091", "myactor", "hello", time.Second)
-
-	// get spawned PID
-	spawnedPID := spawnResponse.Pid
-	context.RequestWithCustomSender(spawnedPID, message, pid)
-	//context.Send(spawnedPID, message)
+	switch flag.Args()[0] {
+	case "newtree":
+		newTree()
+	case "deletetree":
+		if !*forceDelete {
+			fmt.Println("ERROR: Please add force-delete flag for tree deletion")
+			return
+		}
+		deleteTree()
+	case "insertnode":
+		insertNode()
+	case "deletenode":
+		deletenode()
+	case "existsnode":
+		existsnode()
+	case "traversetree":
+		traverseTree()
+	default:
+		printDocumentation()
+		return
+	}
 
 	wg.Wait()
+}
+
+func newTree() {
+	if len(flag.Args()) < 2 {
+		fmt.Printf("ERROR: Please specify the max leaf size as second argument.")
+		wg.Done()
+		return
+	}
+	leafSize, _ := strconv.Atoi(flag.Args()[1])
+	message := &messages.NewTree{MaxLeafSize: int32(leafSize)}
+	context.RequestWithCustomSender(spawnedPID, message, pid)
+}
+
+func deleteTree() {
+	message := &messages.DeleteTree{Token: *token, Id: int32(*id)}
+	context.RequestWithCustomSender(spawnedPID, message, pid)
+}
+
+func insertNode() {
+	if len(flag.Args()) < 2 {
+		fmt.Printf("ERROR: Please specify the value as second argument.")
+		wg.Done()
+		return
+	}
+	message := &messages.NewNumber{Token: *token, Id: int32(*id), Value: flag.Args()[1]}
+	context.RequestWithCustomSender(spawnedPID, message, pid)
+}
+
+func deletenode() {
+	if len(flag.Args()) < 2 {
+		fmt.Printf("ERROR: Please specify the value as second argument.")
+		wg.Done()
+		return
+	}
+	message := &messages.DeleteNumber{Token: *token, Id: int32(*id), Value: flag.Args()[1]}
+	context.RequestWithCustomSender(spawnedPID, message, pid)
+}
+
+func existsnode() {
+	if len(flag.Args()) < 2 {
+		fmt.Printf("ERROR: Please specify the value as second argument.")
+		wg.Done()
+		return
+	}
+	message := &messages.ExistsNumber{Token: *token, Id: int32(*id), Value: flag.Args()[1]}
+	context.RequestWithCustomSender(spawnedPID, message, pid)
+}
+
+func traverseTree() {
+	message := &messages.TraverseTree{Token: *token, Id: int32(*id)}
+	context.RequestWithCustomSender(spawnedPID, message, pid)
+}
+
+func printDocumentation() {
+	fmt.Println("ERROR: Please use the following commands: ")
+	fmt.Printf("newtree\ndeletetree\ninsertnode\ndeletenode\nexistsnode\ntraversetree\n\n")
 }
